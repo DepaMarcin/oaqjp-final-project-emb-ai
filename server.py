58c72291555eb65b29fca5cb147d1a0b6dfd86e4
DepaMarcin <cinek312@interia.pl>
2026-07-05 15:50:18 -0400

    Add error handling to function and server

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from EmotionDetection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector")
+def emotion_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    
+    response = emotion_detector(text_to_analyze)
+    
+    # Krok 3 z instrukcji: Jeśli dominant_emotion to None, zwracamy komunikat o błędzie
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant = response['dominant_emotion']
+    
+    output_text = (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant}."
+    )
+    return output_text
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
